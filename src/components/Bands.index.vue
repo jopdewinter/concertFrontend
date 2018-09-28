c330cc3c3c967b3e64980502281ce4e61d7dc725
jopdewinter2000 <jopdewinter2000@gmail.com>
2018-09-28 13:44:44 +0200

    init

---
+++ src/components/Bands.index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="bands">
+        <div v-for="band in bands" class="band">
+            <h3>{{ band.name }}</h3>
+            <p>Description: {{ band.description }}</p>
+            <p>Origin: {{ band.origin }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Band',
+        data () {
+            return {
+                bands: []
+            }
+        },
+
+        created () {
+            this.$http.get('http://localhost:8000/getAll').then(function(data) {
+                this.bands = data.body
+            })
+        }
+    }
+</script>
+
+<style>
+    .band {
+        width: 40%;
+        background: lightgray;
+        margin: auto;
+        padding: 15px;
+        border-radius: 10px;
+    }
+</style>
---
+++ src/views/Bands.index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="list">
+        <h1>Bands:</h1>
+        <Bands></Bands>
+    </div>
+</template>
+
+<script>
+    import Bands from '@/components/Bands.index.vue'
+    export default  {
+        name: 'index',
+        components: {
+            Bands
+        }
+    }
+</script>
